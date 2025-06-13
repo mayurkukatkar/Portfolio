c4c332020e8b1feca66f9aa415262d4f73542d8a
mayurkukatkar <119735289+mayurkukatkar@users.noreply.github.com>
2025-06-13 12:08:41 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -146,3 +146,6 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    "https://mayurkukatkar.onrender.com"
+]
